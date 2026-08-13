02a3de2c2c18a57217ed7660ee76df06bb0cb55a
shiz2633 <2021s.kiani@gmail.com>
2026-08-13 18:48:12 +0100

    js typewritter
    code for typewriting effect
    changed some html and js to make button work

--- src/script.js
+++ src/script.js
@@ -1,10 +1,16 @@
 function generateCrystal(event) {
   event.preventDefault();
 
-  alert(
-    "Your crystal is being generated! Please wait a few seconds for the image to appear.",
-  );
+  new Typewriter("#results", {
+    strings: "Generating crystal please wait...",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+
+  resultsElement.innerHTML = "Generating crystal...";
 }
 
-let crystalFormElement = document.querySelector("#crystal-generator-form");
-crystalFormElement.addEventListener("submit", generateCrystal);
+document
+  .querySelector("#crystal-generator-form")
+  .addEventListener("submit", generateCrystal);
